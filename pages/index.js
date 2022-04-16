86d6cec98fb36df9661372187d56c3ea908bee64
unknown <kr.vivek.kr@gmail.com>
2022-04-16 13:41:07 +0530

    header_added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-  <div>
-    <h1 className="bg-red-600">Hello test</h1>
-  </div>
-  )
+    <div>
+      <Head>
+        <title>Metaverse Challenge</title>
+        <link rel="icon" href="/favicon.icon" />
+      </Head>
+      <h1>Welcome to the MetaVerse</h1>
+    </div>
+  );
 }
